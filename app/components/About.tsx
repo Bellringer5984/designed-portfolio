a3e3622ba89d1d60b088ffa8cd3ff0a1d085d1a4
cjbellmyer <cjbellmyer5984@gmail.com>
2023-12-11 19:37:44 -0600

    add &apos to about page"

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <section id='about' className="bg-black text-white rounded-lg p-10 my-10 mx-auto max-w-6xl">
+    <section
+      id="about"
+      className="bg-black text-white rounded-lg p-10 my-10 mx-auto max-w-6xl"
+    >
       <h2 className="text-2xl font-bold mb-4">About Me</h2>
       <div className="flex flex-col md:flex-row">
         <div className="md:w-1/3">
@@ -12,13 +15,17 @@ const About = () => {
         <div className="md:w-2/3 md:pl-10">
           <p className="text-lg">
             {/* Insert your biography or introduction here */}
-            Hello! I'm Caleb, a passionate software developer with a knack for building engaging and efficient web applications. With a background in Fullstack Academy and a diverse skill set in JavaScript, TypeScript, Python, and SQL, I am well-equipped to tackle challenging projects and deliver high-quality solutions.
+            Hello! I&apos;m Caleb, a passionate software developer with a knack for
+            building engaging and efficient web applications. With a background
+            in Fullstack Academy and a diverse skill set in JavaScript,
+            TypeScript, Python, and SQL, I am well-equipped to tackle
+            challenging projects and deliver high-quality solutions.
           </p>
           {/* Add more content or paragraphs as needed */}
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
